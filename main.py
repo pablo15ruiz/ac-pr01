b5de65b97a41187522e347129f1a3f1b71834016
Pablo Ruiz <pabloruizhjg@gmail.com>
2021-10-06 19:47:30 +0200

    Clean dataset 1

--- main.py
+++ main.py
@@ -3,3 +3,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats
 import seaborn as sns
+
+# Settings
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+# Load dataset
+path = 'C:/Users/pablo/Documents/TercerUAB/Aprenentatge Computacional/Pràctiques/Pràctica 1/'
+dataset = pd.read_csv(path + 'Life Expectancy Data.csv')
+
+# Correct column names
+dataset = dataset.rename(
+    columns={
+        'Life expectancy ': 'Life expectancy',
+        'infant deaths': 'Infant deaths',
+        'percentage expenditure': 'Percentage expenditure',
+        'Measles ': 'Measles',
+        ' BMI ': 'BMI',
+        'under-five deaths ': 'Under-five deaths',
+        'Diphtheria ': 'Diphtheria',
+        ' HIV/AIDS': 'HIV/AIDS',
+        ' thinness  1-19 years': 'Thinness  1-19 years',
+        ' thinness 5-9 years': 'Thinness 5-9 years'
+        }
+)
+
+print(dataset.columns)
